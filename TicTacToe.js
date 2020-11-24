6dbc4f725bc86a264ced963e46a29c376b20ede9
amandaymxs <amandaxinvestment@gmail.com>
2020-11-23 21:49:51 -0500

    first commit

---
+++ TicTacToe.js
@@ -0,0 +1,10 @@
+let colSm = $(".col-sm");
+let container = $(".container");
+
+window.addEventListener('load', function () {
+    //generate board tiles//
+    $("div.col-sm").addClass("tile");
+
+
+})
+

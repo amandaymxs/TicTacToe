1af438b56d03ec3960a719d49542675755d04e8e
amandaymxs <amandaxinvestment@gmail.com>
2020-11-24 21:19:24 -0500

    isGameOver

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,28 +1,18 @@
 const playerOne = {
     mark: 'X',
-    point: '1'
+    point: 1
 }
 const playerTwo = {
     mark: 'O',
-    point: '-1'
+    point: -1
 }
 
 const board = document.querySelector(".boardContainer");
 const allCells = document.querySelectorAll(".cell");
 const reset = document.querySelector("#reset");
+let scoreCounter = [...Array(8)].fill('0');
 let counter = 0;
-let scoreBoard = [...Array(3)].map(e => Array(3));
 
-let cell = [...Array(8)].map(e => {
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < scoreBoard.lenght; j++) {
-            for (let k = 0; k < j.length; k++) {
-                cell[i] = scoreBoard[j][k];
-
-            }
-        }
-    }
-});
 ///////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
@@ -31,16 +21,15 @@ board.addEventListener("mouseup", (e) => {
     let player = turn();
     if (e.target.innerText == "") {
         e.target.innerText = `${player.mark}`;
-        let cellNum = e.target.id;
-        cell[cellNum] = player.point;
+        scoreCounter[e.target.id] = player.point;
+        console.log(scoreCounter[e.target.id]);
+        if (counter >= 4) {
+            if (isGameOver()) {
+                console.log(`Player ${player.mark} is the winner!`);
+            }
+        }
         counter++;
     }
-
-    // if (counter >= 5) {
-    //     if (isGameOver(player)) {
-    //         console.log(`Player ${player.mark} is the winner!`)
-    //     }
-    // }
 
 })
 
@@ -58,5 +47,11 @@ function turn() {
 }
 
 function isGameOver() {
-
+    for (let i = 0; i < 9; i += 3) {
+        let _sum = scoreCounter[0 + i] + scoreCounter[1 + i] + scoreCounter[2 + i];
+        if (Math.abs(_sum) === 3) {
+            return true;
+        }
+    }
+    return false;
 }
